7cfac9ce236c85fc02620c7efe81fc8dfd4d9db5
Shrey <vaityshrey@gmail.com>
2019-03-07 12:47:42 -0500

    test case fixed

--- testgh.py
+++ testgh.py
@@ -5,7 +5,7 @@ class Testgh(unittest.TestCase):
 	def test_normal(self):
 		self.assertEqual(list(gb("vaityshrey")), [('androcrew', 2)])
 		# used friends github id for testcase below
-		self.assertEqual(list(gb("svaity")), [('567Github', 15), ('Data-Visualization-Based-on-Emails-Day-From-Log', 4), ('Distance-Calculator-Using-Web-APIs-JSON', 3), ('Ecommerce-Framwork', 15), ('PHP-Login-System', 4), ('Portfolio-using-Django-Postgres', 5), ('Reduce-Noise-in-LiDAR-sensor', 8), ('Stevens_Repository', 7), ('Travel_Guide-UI-Wireframe-', 3), ('Triangle567', 8)])
+		self.assertEqual(list(gb("svaity")), [('567Github', 2), ('Data-Visualization-Based-on-Emails-Day-From-Log', 4), ('Distance-Calculator-Using-Web-APIs-JSON', 3), ('Ecommerce-Framwork', 15), ('PHP-Login-System', 4), ('Portfolio-using-Django-Postgres', 5), ('Reduce-Noise-in-LiDAR-sensor', 8), ('Stevens_Repository', 7), ('Travel_Guide-UI-Wireframe-', 3), ('Triangle567', 8)])
 
 if __name__ == '__main__':
 	print('Running unit tests')
